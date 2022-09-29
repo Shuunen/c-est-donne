ab3de6a80b9d486e9a4af8a9604c5764e2e12d72
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-09-29 20:45:13 +0200

    setup dark & light themes with tailwind, shoelace & custom vars

--- src/plugins/shoelace.ts
+++ src/plugins/shoelace.ts
@@ -1,6 +1,7 @@
 import '@shoelace-style/shoelace/dist/components/button/button.js'
 import '@shoelace-style/shoelace/dist/components/input/input.js'
 import '@shoelace-style/shoelace/dist/themes/dark.css'
+import '@shoelace-style/shoelace/dist/themes/light.css'
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path'
 
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.82/dist/')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,15 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // https://tailwindcss.com/docs/customizing-colors#using-css-variables
+        accent: 'hsl(var(--color-accent) / <alpha-value>)',
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 }
