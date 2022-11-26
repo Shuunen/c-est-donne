a5df813949dc6d19820d15aaf7675b7e600a8d5f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-11-26 17:40:05 +0100

    handle mobile display

--- src/components/items-list-header.vue
+++ src/components/items-list-header.vue
@@ -10,30 +10,33 @@ import { Tab, type Display, type Filter } from '../utils/tabs'
 
 const { t } = useI18n()
 const counts = ref({ [Tab.Available]: 0, [Tab.ReservedByMe]: 0, [Tab.All]: 0 })
-const filter = ref<Filter>(Tab.Available)
 
 function listSort (itemA: Item, itemB: Item): number {
   log('sorting items')
   // sort by availability
-  if ([Tab.ReservedByMe, Tab.Available].includes(filter.value)) return Number(itemB.isVisible) - Number(itemA.isVisible)
+  if ([Tab.ReservedByMe, Tab.Available].includes(state.filter)) return Number(itemB.isVisible) - Number(itemA.isVisible)
   // or by name by default
   return itemA.name > itemB.name ? 1 : -1 // eslint-disable-line @typescript-eslint/no-magic-numbers
 }
 
-// eslint-disable-next-line sonarjs/cognitive-complexity
-function showTab (name: Tab): void {
-  log('showing tab', name)
-  if ([Tab.Available, Tab.ReservedByMe, Tab.All].includes(name)) {
-    filter.value = name as Filter // eslint-disable-line @typescript-eslint/consistent-type-assertions
-    if (name === Tab.Available) for (const item of state.items) item.isVisible = item.status === ItemStatus.Available
-    else if (name === Tab.ReservedByMe) for (const item of state.items) item.isVisible = item.status === ItemStatus.ReservedByMe
-    else if (name === Tab.All) for (const item of state.items) item.isVisible = true
-    else throw new Error('unhandled case')
-    // eslint-disable-next-line etc/no-assign-mutated-array
-    state.items = state.items.sort(listSort)
-  } else if ([Tab.List, Tab.Cards].includes(name))
-    state.display = name as Display // eslint-disable-line @typescript-eslint/consistent-type-assertions
-  else error('an-error-occurred', `un-handled tab name "${name}"`)
+function onFilter (): void {
+  log('on filter :', state.filter)
+  if (state.filter === Tab.Available) for (const item of state.items) item.isVisible = item.status === ItemStatus.Available
+  else if (state.filter === Tab.ReservedByMe) for (const item of state.items) item.isVisible = item.status === ItemStatus.ReservedByMe
+  else for (const item of state.items) item.isVisible = true
+  // eslint-disable-next-line etc/no-assign-mutated-array
+  state.items = state.items.sort(listSort)
+}
+
+function onDisplay (): void {
+  log('on display :', state.display)
+}
+
+function onTab (tab: Tab): void {
+  log('on tab :', tab)
+  if ([Tab.Available, Tab.ReservedByMe, Tab.All].includes(tab)) state.filter = tab as Filter // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  else if ([Tab.List, Tab.Cards].includes(tab)) state.display = tab as Display // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  else error('an-error-occurred', `un-handled tab name "${state.filter}"`)
 }
 
 function refreshCounts (): void {
@@ -45,28 +48,56 @@ function refreshCounts (): void {
 function onItems (): void {
   log('updating items list')
   refreshCounts()
-  showTab(filter.value)
+  onFilter()
 }
 
-on('sl-tab-show', ({ name }: { name: Tab }): void => { showTab(name) })
+function labelFor (tab: Tab): string {
+  if (tab === Tab.Available) return t('tab-available', { count: counts.value[Tab.Available] })
+  if (tab === Tab.ReservedByMe) return t('tab-reserved-by-me', { count: counts.value[Tab.ReservedByMe] })
+  if (tab === Tab.All) return t('tab-all', { count: counts.value[Tab.All] })
+  if (tab === Tab.List) return t('display-list')
+  return t('display-card')
+}
+
+on('sl-tab-show', ({ name }: { name: Tab }): void => { onTab(name) })
 
 watch(() => state.items, onItems)
+watch(() => state.filter, onFilter)
+watch(() => state.display, onDisplay)
 </script>
 
 <template>
-  <div v-if="state.user.email" class="app-items-list--header hidden sm:flex flex-row flex-wrap justify-end items-end">
-    <sl-tab-group class="grow">
-      <sl-tab slot="nav" panel="available">{{ t('tab-available', { count: counts[Tab.Available] }) }}</sl-tab>
-      <sl-tab slot="nav" panel="reserved-by-me">{{ t('tab-reserved-by-me', { count: counts[Tab.ReservedByMe] }) }}</sl-tab>
-      <sl-tab slot="nav" panel="all">{{ t('tab-all', { count: counts[Tab.All] }) }}</sl-tab>
+  <div v-if="state.user.email" class="app-items-list--header sm:flex flex-row flex-wrap justify-end items-end">
+    <sl-dropdown class="sm:hidden ml-auto">
+      <sl-button slot="trigger" caret>
+        <template v-if="state.filter === Tab.Available">{{ labelFor(Tab.Available) }}</template>
+        <template v-else-if="state.filter === Tab.ReservedByMe">{{ labelFor(Tab.ReservedByMe) }}</template>
+        <template v-else-if="state.filter === Tab.All">{{ labelFor(Tab.All) }}</template>
+      </sl-button>
+      <sl-menu>
+        <sl-menu-item v-if="state.filter !== Tab.Available" @click="state.filter = Tab.Available">
+          {{ labelFor(Tab.Available) }}
+        </sl-menu-item>
+        <sl-menu-item v-if="state.filter !== Tab.ReservedByMe" @click="state.filter = Tab.ReservedByMe">
+          {{ labelFor(Tab.ReservedByMe) }}
+        </sl-menu-item>
+        <sl-menu-item v-if="state.filter !== Tab.All" @click="state.filter = Tab.All">
+          {{ labelFor(Tab.All) }}
+        </sl-menu-item>
+      </sl-menu>
+    </sl-dropdown>
+    <sl-tab-group class="grow hidden sm:block">
+      <sl-tab slot="nav" panel="available">{{ labelFor(Tab.Available) }}</sl-tab>
+      <sl-tab slot="nav" panel="reserved-by-me">{{ labelFor(Tab.ReservedByMe) }}</sl-tab>
+      <sl-tab slot="nav" panel="all">{{ labelFor(Tab.All) }}</sl-tab>
     </sl-tab-group>
-    <sl-tab-group>
+    <sl-tab-group class="hidden sm:block">
       <sl-tab slot="nav" panel="list">
-        {{ t('display-list') }}
+        {{ labelFor(Tab.List) }}
         <sl-icon name="list" class="ml-3 mt-1"></sl-icon>
       </sl-tab>
       <sl-tab slot="nav" panel="cards">
-        {{ t('display-card') }}
+        {{ labelFor(Tab.Cards) }}
         <sl-icon name="card-heading" class="ml-3 mt-1"></sl-icon>
       </sl-tab>
     </sl-tab-group>
