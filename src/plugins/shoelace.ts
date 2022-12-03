2263d2e5d474b41229e9f7594af10ededdfa7891
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-12-03 19:23:48 +0100

    update icon cdn

--- src/plugins/shoelace.ts
+++ src/plugins/shoelace.ts
@@ -15,4 +15,5 @@ import '@shoelace-style/shoelace/dist/themes/dark.css'
 import '@shoelace-style/shoelace/dist/themes/light.css'
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path'
 
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.82/dist/')
+// used for icons
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.85/dist/')
